714a0321f8ea1604946196876a193bc82c3d76bf
singh ankit <toolate.2025.cp@gmail.com>
2025-03-29 03:55:00 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (78.42%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -3,14 +3,13 @@ public:
     int removeDuplicates(vector<int>& nums) {
   int n=nums.size();
   int i=0;
-
   for(int j=i+1;j<n;j++){
     if(nums[j]!=nums[i]){
-       nums[i+1]=nums[j];
-       i++;
+        nums[i+1]=nums[j];
+        i++;
     }
   }
-  return i+1;
+return i+1;
     }
  
 };
